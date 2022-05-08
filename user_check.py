1e24675e14f7bcdc782fa6eccba29d8cd1e1998e
DuTra01 <glemyson20@gmail.com>
2022-05-08 20:08:07 -0300

    update version

--- user_check.py
+++ user_check.py
@@ -10,7 +10,7 @@ from datetime import datetime
 from flask import Flask, jsonify
 
 __author__ = '@DuTra01'
-__version__ = '0.0.5'
+__version__ = '0.1.1'
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
